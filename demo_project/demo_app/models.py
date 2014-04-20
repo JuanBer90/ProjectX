aaa1f3595de7f3efa7d979c4e6243b27c4ec409e
gerardo <griveros@pol.una.py>
2014-04-19 21:10:43 -0400

    Modelo Proyectos

---
+++ demo_project/demo_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Proyecto(models.Model):
+
+    class Meta:
+        db_table='proyectos'
+
+    id_proyecto=models.AutoField(primary_key=True)
+    leader=models.OneToOneField(User, primary_key=True)
+    nombre = models.CharField(max_length=30, unique=True)
+    descripcion= models.TextField(max_length=300, help_text='Descripcion del Proyecto')
+    fecha_creacion= models.DateTimeField(auto_created=True)
+    complejidad=models.IntegerField(default=0)
+    estado=models.BooleanField(default=True)
+    nro_fases=models.IntegerField()
+    coste_total = models.IntegerField()
