4bc8ff50df86616903455524870cbc6e0f667d90
CarlosRiquelme <tato26.09@gmail.com>
2014-11-09 18:25:52 -0300

    Reestructuracion del Proyecto

--- bootstrap_toolkit/templatetags/my_tags.py
+++ bootstrap_toolkit/templatetags/my_tags.py
@@ -1,20 +1,14 @@
 from django.template.base import Node
-from demo_project.demo_app.models import Cambio
 from django import template
+from AdminProyectos.models import Proyecto
 register=template.Library()
 
-class CambioActual(Node):
-    def render(self, context):
-        cont_=Cambio.objects.filter(id_cambio=1).count()
-        print 'cont: '+str(cont_)
-        if cont_ !=0:
-            cambio =Cambio.objects.get(pk=1)
-        else:
-             cambio=Cambio()
-             cambio.monto=0
-        return cambio.monto
 
-def get_cambio(parser, token):
-    return CambioActual()
-get_cambio = register.tag(get_cambio)
-
+#===============================================================================
+# def get_cambio(parser, token):
+#     return CambioActual()
+# get_cambio = register.tag(get_cambio)
+#===============================================================================
+@register.filter
+def mis_proyectos(id_user):
+    return Proyecto.objects.filter(leader_id=id_user)
--- demo_project/demo_app/AdminGestionCambios/cambio.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from demo_project.demo_app.models import Item
-from django.template.context import RequestContext
-
-def cambio(request):
-    item_list = Item.objects.filter(estado='BLOQUEADO')
-    return render_to_response('HtmlGestionCambio/gestioncambio.html',{'item':item_list}, RequestContext(request))
-
-
